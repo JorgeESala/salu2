b8a1a3fcd8036c5e89c2452b59382db1480c1b4a
Jorge Sala <jorge.e.sala@gmail.com>
2024-03-05 00:12:41 -0500

    feat: agregue la temática de shrek a la página

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 function agregarNombre(){
-    const nombre = prompt("Ingrese su nombre");
+    const nombre = document.getElementById("input-name").value;
     document.getElementById("saludo").innerHTML= `¡Hola ${nombre}!`;
+    document.getElementById("shrek-img").src = "./assets/img/saludando-modificado.png"
+}
+
+function reiniciarSaludo(){
+    document.getElementById("saludo").innerHTML= `¿Quién eres?`;
+    document.getElementById("shrek-img").src = "./assets/img/asustando.png"
 }
